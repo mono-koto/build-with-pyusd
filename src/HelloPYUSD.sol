41c88c518f522242e9aee48eede4b17715fb6590
Brent Fitzgerald <brent@mono-koto.com>
2024-03-12 12:08:14 -0700

    accept ERC-20 token for mint payment

--- src/HelloPYUSD.sol
+++ src/HelloPYUSD.sol
@@ -2,17 +2,25 @@
 pragma solidity 0.8.23;
 
 import {ERC721} from "solmate/tokens/ERC721.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
 
 contract HelloPYUSD is ERC721 {
     uint256 public totalIssued;
 
-    constructor() ERC721("HelloPYUSD", "HIPYPL") {}
+    ERC20 public immutable mintToken;
+    uint256 public immutable mintPrice;
+
+    constructor(address _mintToken, uint256 _mintPrice) ERC721("HelloPYUSD", "HIPYPL") {
+        mintToken = ERC20(_mintToken);
+        mintPrice = _mintPrice;
+    }
 
     function mint() external {
+        mintToken.transferFrom(msg.sender, address(this), mintPrice);
         _mint(msg.sender, ++totalIssued);
     }
 
-    function tokenURI(uint256 id) public view override returns (string memory) {
+    function tokenURI(uint256) public pure override returns (string memory) {
         return "";
     }
 }
--- test/HelloPYUSD.t.sol
+++ test/HelloPYUSD.t.sol
@@ -1,14 +1,19 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {HelloPYUSD} from "../src/HelloPYUSD.sol";
+import {MockPYUSD} from "./MockPYUSD.sol";
 
 contract HelloPYUSDTest is Test {
+    MockPYUSD public pyusd;
     HelloPYUSD public helloPYUSD;
 
+    uint256 constant MINT_PRICE = 100e6;
+
     function setUp() public {
-        helloPYUSD = new HelloPYUSD();
+        pyusd = new MockPYUSD();
+        helloPYUSD = new HelloPYUSD(address(pyusd), MINT_PRICE);
     }
 
     function testCreate() public {
@@ -16,8 +21,24 @@ contract HelloPYUSDTest is Test {
     }
 
     function testMint() public {
+        pyusd.mint(address(this), MINT_PRICE);
+        pyusd.approve(address(helloPYUSD), MINT_PRICE);
+
         helloPYUSD.mint();
+
+        assertEq(pyusd.balanceOf(address(this)), 0);
+        assertEq(pyusd.balanceOf(address(helloPYUSD)), MINT_PRICE);
+
         assertEq(helloPYUSD.balanceOf(address(this)), 1);
         assertEq(helloPYUSD.totalIssued(), 1);
+    }
+
+    function testFail_MintWithoutApproval() public {
+        helloPYUSD.mint();
+    }
+
+    function testFail_MintWithInsufficientBalance() public {
+        pyusd.approve(address(helloPYUSD), MINT_PRICE);
+        helloPYUSD.mint();
     }
 }
