f92f3105b5e1603e0e18633a36b28e67cdfddbea
Brent Fitzgerald <brent@mono-koto.com>
2024-03-20 14:29:42 -0700

    add react-hot-toast

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "@rainbow-me/rainbowkit/styles.css";
 
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 import { WagmiProvider } from "wagmi";
 import HelloPyusd from "./components/HelloPyusd";
 import { wagmiConfig } from "./config";
@@ -14,6 +15,16 @@ export default function App() {
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
           <HelloPyusd />
+          <Toaster
+            toastOptions={{
+              position: "bottom-right",
+              duration: 5000,
+              style: {
+                background: "#666",
+                color: "#ccc",
+              },
+            }}
+          />
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
